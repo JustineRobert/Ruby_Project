03a778ac919eb5273f48fa9ee00620cdf7f8fe92
Justine Robert Igune <igunejustinerobert@gmail.com>
2020-07-18 16:30:52 +0300

    Ruby on Rails First Project

---
+++ helloworldmethod.rb
@@ -0,0 +1,7 @@
+def hello(anything)
+  puts anything
+end
+
+hello "My name is Igune Justine Robert"
+hello "Welcome to a complete Ruby on Rails Developers Course!"
+hello "Are you ready to learn something more Challenging in the beginning and yet it's a great tool for applications' development?"
---
+++ helloworldstring.rb
@@ -0,0 +1,6 @@
+hello ="Hello World!"
+
+puts hello
+
+hello = "Welcome to a complete Ruby on Rails Developer Course by Justine Robert Igune!"
+puts hello
